3b379455ebaaa0ead5da674ca80ec895c2ad1719
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-03-28 21:33:59 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import "../styles/Header.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Hoppy Beaver Brewery Employee Directory</h1>
+        <p>Click on column headings to filter by heading or use the search box to narrow your results.</p>
+      </div>
+    )
+  }
+
+export default Header;
