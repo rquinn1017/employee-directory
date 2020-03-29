e76cba594896f48213def1d14210f098fd04f950
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-03-28 20:56:52 -0400

    added created employee area and table based on random user api fields

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeArea from "./EmployeeArea";
+import "../styles/Main.css";
+
+function Main () {
+    return (
+      <>
+        <EmployeeArea />
+      </>
+    );
+  };
+
+export default Main;
